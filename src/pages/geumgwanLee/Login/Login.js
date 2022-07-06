76810d084a2a8765ed1733def7f973e35df6275d
이금관 <igeumgwan@eric.local>
2022-07-06 15:27:40 +0900

    add: Login.js

--- src/pages/geumgwanLee/Login/Login.js
+++ src/pages/geumgwanLee/Login/Login.js
@@ -2,6 +2,40 @@ import React from 'react';
 import './Login.scss';
 
 function LoginGeumGwan() {
-  return <div className="loginGeumGwan" />;
+  return (
+    <div class="container">
+      <header>
+        <h2>Westagram</h2>
+      </header>
+      <main>
+        <div>
+          <input
+            type="text"
+            id="id"
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+            class="inputlogin"
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            id="pw"
+            placeholder="비밀번호"
+            class="inputlogin"
+          />
+        </div>
+        <div className="buttonarea">
+          <button type="submit" id="submit">
+            로그인
+          </button>
+        </div>
+      </main>
+      <footer>
+        {' '}
+        <a href="">비밀번호를 잊으셨나요?</a>
+      </footer>
+    </div>
+  );
 }
+
 export default LoginGeumGwan;
