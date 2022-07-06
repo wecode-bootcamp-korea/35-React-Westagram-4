271faf24be1482b066d54810a876ec83118c1fe1
wjddms4107 <78889402+wjddms4107@users.noreply.github.com>
2022-07-06 15:27:57 +0900

    login, main 페이지 푸쉬

--- src/pages/jeongeunNoh/Login/Login.js
+++ src/pages/jeongeunNoh/Login/Login.js
@@ -1,7 +1,37 @@
 import React from 'react';
 import './Login.scss';
 
+import { Link, useNavigate } from 'react-router-dom';
+
 function LoginJeongEun() {
-  return <div className="loginJeongEun" />;
+  const navigate = useNavigate();
+
+  const goToMain = () => {
+    navigate('/main');
+  };
+
+  return (
+    <div className="Login">
+      <Link to="/main" style={{ textDecoration: 'none' }}>
+        <h1 className="logo">westagram</h1>
+      </Link>
+      <input
+        className="id"
+        type="text"
+        placeholder="&nbsp;&nbsp;전화번호, 사용자 이름 또는 이메일"
+      />
+      <input
+        className="password"
+        type="password"
+        placeholder="&nbsp;&nbsp;비밀번호"
+      />
+      <button type="button" onClick={goToMain}>
+        로그인
+      </button>
+      <a href="/" className="notice">
+        비밀번호를 잊으셨나요?
+      </a>
+    </div>
+  );
 }
 export default LoginJeongEun;
--- src/pages/jeongeunNoh/Main/Main.js
+++ src/pages/jeongeunNoh/Main/Main.js
@@ -2,6 +2,212 @@ import React from 'react';
 import './Main.scss';
 
 function MainJeongEun() {
-  return <div className="mainJeongEun" />;
+  return (
+    <>
+      <nav>
+        <div className="navBar">
+          <div className="leftMenu">
+            <img
+              className="instagram"
+              src="./images/instagram.png"
+              alt="instagram"
+            />
+            <span id="westagram">westagram</span>
+          </div>
+
+          <div className="centerMenu">
+            <input
+              className="centerMenuInput"
+              type="text"
+              placeholder="&nbsp;&nbsp;&nbsp;&nbsp;검색"
+            />
+            <span className="magnify">
+              <i className="fa-solid fa-magnifying-glass" />
+            </span>
+          </div>
+
+          <div className="rightMenu">
+            <ul className="menu">
+              <li>
+                <img src="./images/explore.png" alt="explore" />
+              </li>
+              <li className="li2">
+                <img src="./images/heart.png" alt="heart" />
+                <span>.</span>
+              </li>
+              <li>
+                <img src="./images/profile.png" alt="profile" />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <main>
+        <div className="feeds">
+          <article className="mainFeed">
+            <div className="mainFeedBar">
+              <img src="./images/img1.jpeg" alt="circleimg" />
+              <span>
+                <strong>jeongeuni___</strong>
+              </span>
+              <i className="fa-solid fa-ellipsis" />
+            </div>
+            <div className="mainFeedImg">
+              <img src="./images/img2.jpeg" alt="mainfeedimg" />
+            </div>
+            <div className="mainFeedContents">
+              <div className="contentsIcons">
+                <span className="contentsIconsLeft">
+                  <i className="fa-solid fa-heart" />
+                  <i className="fa-regular fa-comment" />
+                  <i className="fa-solid fa-arrow-up-from-bracket" />
+                </span>
+                <span className="contentsIconsRight">
+                  <i className="fa-regular fa-bookmark" />
+                </span>
+              </div>
+              <div className="contentsComments">
+                <div className="like">
+                  <img src="./images/img3.jpeg" alt="" />
+                  <span className="likeComment">
+                    <strong className="likers">duck</strong>님 외{' '}
+                    <strong>1000명</strong>이 좋아합니다
+                  </span>
+                </div>
+                <div className="myComment">
+                  <strong>jeongeuni___</strong>&nbsp;&nbsp;나의 사랑 다콩이
+                  드디어 접선...<span className="weight">&nbsp;더 보기</span>
+                </div>
+              </div>
+              <div className="clockFlow">
+                <span className="weight">1시간 전</span>
+              </div>
+            </div>
+          </article>
+
+          <div className="mainFeedComment">
+            <input
+              className="inputComment"
+              type="text"
+              placeholder="댓글 달기..."
+            />
+            <input className="inputSubmit" type="submit" value="게시" />
+          </div>
+        </div>
+
+        <div className="mainRight">
+          <div className="myInsta">
+            <img src="./images/myInsta.jpeg" alt="myInsta" />
+            <div className="myId">
+              <p>
+                <strong>jeongeuni__</strong>
+              </p>
+              <p className="weight">노정은</p>
+            </div>
+          </div>
+
+          <div className="storyBox">
+            <div className="storysVar">
+              <span className="weight">스토리</span>
+              <span>
+                <strong>모두 보기</strong>
+              </span>
+            </div>
+
+            <div className="storys">
+              <div className="story">
+                <img src="./images/right1.jpeg" alt="myInsta" />
+                <div className="storyId">
+                  <p>
+                    <strong>jeongeuni__</strong>
+                  </p>
+                  <p className="weight">노정은</p>
+                </div>
+              </div>
+              <div className="story">
+                <img src="./images/right2.jpeg" alt="myInsta" />
+                <div className="storyId">
+                  <p>
+                    <strong>jeongeuni__</strong>
+                  </p>
+                  <p className="weight">노정은</p>
+                </div>
+              </div>
+              <div className="story">
+                <img src="./images/right3.jpeg" alt="myInsta" />
+                <div className="storyId">
+                  <p>
+                    <strong>jeongeuni__</strong>
+                  </p>
+                  <p className="weight">노정은</p>
+                </div>
+              </div>
+              <div className="story">
+                <img src="./images/right4.jpeg" alt="myInsta" />
+                <div className="storyId">
+                  <p>
+                    <strong>jeongeuni__</strong>
+                  </p>
+                  <p className="weight">노정은</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="freindRecommendBox">
+            <div className="recommendVar">
+              <span className="weight">회원님을 위한 추천</span>
+              <span>
+                <strong>모두 보기</strong>
+              </span>
+            </div>
+
+            <div className="recommendPeopel">
+              <img src="./images/right5.jpeg" alt="myInsta" />
+              <div className="recommendId">
+                <p>
+                  <strong>jeongeuni__</strong>
+                </p>
+                <p className="weight">_legend_a님 외 2명이...</p>
+              </div>
+              <span className="follow">팔로우</span>
+            </div>
+
+            <div className="recommendPeopel">
+              <img src="./images/right6.jpeg" alt="myInsta" />
+              <div className="recommendId">
+                <p>
+                  <strong>jeongeuni__</strong>
+                </p>
+                <p className="weight">ringo.in.seoul님 외 12...</p>
+              </div>
+              <span className="follow">팔로우</span>
+            </div>
+
+            <div className="recommendPeopel">
+              <img src="./images/right7.jpeg" alt="myInsta" />
+              <div className="recommendId">
+                <p>
+                  <strong>jeongeuni__</strong>
+                </p>
+                <p className="weight">jimmtlee1220님 외 1...</p>
+              </div>
+              <span className="follow">팔로우</span>
+            </div>
+          </div>
+
+          <div className="footer">
+            <p>instagram 정보 ・ 지원 ・ 홍보 센터 ・ API ・ </p>
+            <p>채용 정보 ・ 개인정보처리방침 ・ 약관 ・ </p>
+            <p>디렉터리 ・ 프로필 ・ 해시태그 ・ 언어 </p>
+            <br />
+            <p>© 2019 INSTAGRAM</p>
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
+
 export default MainJeongEun;
