54908848329d41cdbced4f7e6c20db284f0a742c
정억화 <jeong-eoghwa@jeong-eoghwaui-MacBookPro.local>
2022-07-06 15:27:33 +0900

    add: 로그인페이지 업데이트

--- src/pages/eokhwaJeong/Login/Login.js
+++ src/pages/eokhwaJeong/Login/Login.js
@@ -2,6 +2,33 @@ import React from 'react';
 import './Login.scss';
 
 function LoginEokHwa() {
-  return <div className="loginEokHwa" />;
+  return (
+    <div className="login-eokhwa">
+      <div className="logo">
+        <p>westagram</p>
+      </div>
+      <div className="input_box">
+        <input
+          type="text"
+          id="id_input"
+          placeholder="전화번호, 사용자 이름 또는 이메일"
+          onkeyup="isCheckIdAndPass()"
+        />
+        <input
+          type="password"
+          id="pass_input"
+          placeholder="비밀번호"
+          onkeyup="isCheckIdAndPass()"
+        />
+
+        <div>
+          <button className="on">로그인</button>
+        </div>
+      </div>
+      <a href="#!" className="last_txt">
+        <p className="">비밀번호를 잊으셨나요?</p>
+      </a>
+    </div>
+  );
 }
 export default LoginEokHwa;
