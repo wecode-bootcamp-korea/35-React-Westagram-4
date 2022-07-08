ad5d7bf8fc52faa9b7dd1936a056e005d88afdbc
정억화 <jeong-eoghwa@jeong-eoghwaui-MacBookPro.local>
2022-07-08 15:32:19 +0900

    로그인페이지 로직구성

--- src/pages/eokhwaJeong/Login/Login.js
+++ src/pages/eokhwaJeong/Login/Login.js
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.scss';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
+const Button = () => {
+  return (
+    <button className="" disabled>
+      로그인
+    </button>
+  );
+};
+
+const ButtonActive = () => {
+  return (
+    <button className="on" disabled="false">
+      로그인
+    </button>
+  );
+};
 
 function LoginEokHwa() {
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
+  const [display, setDisplay] = useState('flex');
+  const isValid = id.includes('@') && pw.length > 5;
+  const navigate = useNavigate();
+
+  const goToMain = () => {
+    navigate('/main-eokhwa');
+  };
+  // const nowUrl = window.location.href;
+  // const checkUrl = nowUrl.includes('login');
+
+  // const switchNav = e => {
+  //   if (e) {
+  //     setDisplay('none');
+  //   } else {
+  //     setDisplay('flex');
+  //   }
+  // };
+
+  // console.log(display);
+  // console.log(checkUrl);
   return (
     <div className="login-eokhwa">
       <div className="logo">
@@ -13,19 +51,23 @@ function LoginEokHwa() {
           type="text"
           id="id_input"
           placeholder="전화번호, 사용자 이름 또는 이메일"
-          onkeyup="isCheckIdAndPass()"
+          onChange={e => setId(e.target.value)}
         />
         <input
           type="password"
           id="pass_input"
           placeholder="비밀번호"
-          onkeyup="isCheckIdAndPass()"
+          onChange={e => setPw(e.target.value)}
         />
 
-        <div>
-          <Link to="/main-eokhwa">
-            <button className="on">로그인</button>
-          </Link>
+        <div className="button_area">
+          <button
+            className={isValid ? 'on' : ' '}
+            disabled={isValid ? false : true}
+            onClick={goToMain}
+          >
+            로그인
+          </button>
         </div>
       </div>
       <a href="#!" className="last_txt">
