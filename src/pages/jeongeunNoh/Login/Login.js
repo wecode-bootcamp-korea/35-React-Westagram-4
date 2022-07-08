707098243b6b0683323d0cccd0666a2523ab93a6
wjddms4107 <78889402+wjddms4107@users.noreply.github.com>
2022-07-08 17:55:14 +0900

    Add : Mission 4 - 댓글 컴포넌트화, props로 데이터 전달 구현

--- src/pages/jeongeunNoh/Login/Login.js
+++ src/pages/jeongeunNoh/Login/Login.js
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.scss';
-
 import { Link, useNavigate } from 'react-router-dom';
 
 function LoginJeongEun() {
@@ -10,22 +9,45 @@ function LoginJeongEun() {
     navigate('/main-jeongeun');
   };
 
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
+
+  const isTrue = id.includes('@') && pw.length >= 5;
+
+  const handleIdInput = e => {
+    // console.log(e.target.value)
+    setId(e.target.value); // 이 값이 계속 id로 들어가고 있음
+  };
+
+  const handlePwInput = e => {
+    setPw(e.target.value); // 이 값이 계속 pw로 들어가고 있음
+  };
+
   return (
     <div className="Login">
-      <Link to="/main-jeongeun" style={{ textDecoration: 'none' }}>
+      <Link to="/main" style={{ textDecoration: 'none' }}>
         <h1 className="logo">westagram</h1>
       </Link>
       <input
+        onChange={handleIdInput}
         className="id"
         type="text"
-        placeholder="&nbsp;&nbsp;전화번호, 사용자 이름 또는 이메일"
+        placeholder="전화번호, 사용자 이름 또는 이메일"
       />
       <input
+        onChange={handlePwInput}
         className="password"
         type="password"
-        placeholder="&nbsp;&nbsp;비밀번호"
+        placeholder="비밀번호"
       />
-      <button type="button" onClick={goToMain}>
+      <button
+        type="button"
+        onClick={goToMain}
+        style={
+          isTrue ? { backgroundColor: 'blue' } : { backgroundColor: '#BEE2FD' }
+        }
+        disabled={!isTrue}
+      >
         로그인
       </button>
       <a href="/" className="notice">
--- src/pages/jeongeunNoh/Main/Main.js
+++ src/pages/jeongeunNoh/Main/Main.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Main.scss';
+import MainFeed from '../../../components/MainFeed/MainFeed';
 
 function MainJeongEun() {
+  let [value, setValue] = useState('');
+  let [comment, setComment] = useState([]);
+
+  const getInputValue = e => {
+    setValue(e.target.value);
+  };
+
+  const addComment = e => {
+    if (e.key === 'Enter') {
+      const copyComment = [...comment]; //[인풋값들이 담겨있음]
+      copyComment.push(value); //value = e.target.value
+      // console.log(copyComment) //copyComment = [value, value, value]
+      setComment(copyComment);
+      console.log(comment); // comment = [추가, 추가, 추가]
+      setValue('');
+      e.preventDefault();
+    }
+  };
+
   return (
     <>
-      <nav>
+      {/* <nav>
         <div className="navBar">
           <div className="leftMenu">
             <img
@@ -41,59 +61,31 @@ function MainJeongEun() {
             </ul>
           </div>
         </div>
-      </nav>
+      </nav> */}
 
       <main>
         <div className="feeds">
-          <article className="mainFeed">
-            <div className="mainFeedBar">
-              <img src="/images/jeongeunNoh/img1.jpeg" alt="circleimg" />
-              <span>
-                <strong>jeongeuni___</strong>
-              </span>
-              <i className="fa-solid fa-ellipsis" />
-            </div>
-            <div className="mainFeedImg">
-              <img src="/images/jeongeunNoh/img2.jpeg" alt="mainfeedimg" />
-            </div>
-            <div className="mainFeedContents">
-              <div className="contentsIcons">
-                <span className="contentsIconsLeft">
-                  <i className="fa-solid fa-heart" />
-                  <i className="fa-regular fa-comment" />
-                  <i className="fa-solid fa-arrow-up-from-bracket" />
-                </span>
-                <span className="contentsIconsRight">
-                  <i className="fa-regular fa-bookmark" />
-                </span>
-              </div>
-              <div className="contentsComments">
-                <div className="like">
-                  <img src="/images/jeongeunNoh/img3.jpeg" alt="" />
-                  <span className="likeComment">
-                    <strong className="likers">duck</strong>님 외{' '}
-                    <strong>1000명</strong>이 좋아합니다
-                  </span>
-                </div>
-                <div className="myComment">
-                  <strong>jeongeuni___</strong>&nbsp;&nbsp;나의 사랑 다콩이
-                  드디어 접선...<span className="weight">&nbsp;더 보기</span>
-                </div>
-              </div>
-              <div className="clockFlow">
-                <span className="weight">1시간 전</span>
-              </div>
-            </div>
-          </article>
+          {/* {comment.map((data, i) => {
+          return <MainFeed key={i} data={data} index={i} />
+        })} */}
 
-          <div className="mainFeedComment">
+          {/* comment = [추가, 추가, 추가] */}
+          <MainFeed comment={comment} />
+
+          <form className="mainFeedComment">
+            {/* onSubMit={addComment} */}
+
             <input
               className="inputComment"
               type="text"
               placeholder="댓글 달기..."
+              value={value}
+              onChange={getInputValue}
+              onKeyPress={addComment}
             />
-            <input className="inputSubmit" type="submit" value="게시" />
-          </div>
+
+            <input type="button" className="inputSubmit" value="게시" />
+          </form>
         </div>
 
         <div className="mainRight">
