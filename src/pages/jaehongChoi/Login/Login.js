fc42ae1583137c70d40c75fe83196dac22a31fb2
최재홍 <choejaehong@choejaehong-ui-MacBookPro.local>
2022-07-09 18:11:59 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/jaehongChoi/Login/Login.js
+++ src/pages/jaehongChoi/Login/Login.js
@@ -1,42 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.scss';
 import { Link } from 'react-router-dom';
 
 function LoginJaeHong() {
   return (
     <div className="login-jaeHong">
-      <div class="wholePage">
-        <div class="loginBox">
-          <div class="logoBox">
+      <div className="wholePage">
+        <div className="loginBox">
+          <div className="logoBox">
             <Link to="/main-jaehong">
               <h1>Westagram</h1>
             </Link>
           </div>
-          <div class="loginElement">
-            <div class="inputBlank">
-              <div class="idDiv">
-                <input
-                  class="id"
-                  type="text"
-                  placeholder="전화번호, 사용자 이름 또는 이메일"
-                />
-              </div>
-              <div class="passworddiv">
-                <input
-                  class="password"
-                  type="password"
-                  placeholder="비밀번호"
-                />
-              </div>
-              <button type="button" class="button">
-                로그인
-              </button>
-            </div>
-          </div>
+          <LoginElement />
+          <p>비밀번호를 잊으셨나요?</p>
         </div>
-        <p>비밀번호를 잊으셨나요?</p>
       </div>
     </div>
   );
 }
+
+const LoginElement = () => {
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleIdInput = event => {
+    setId(event.target.value);
+  };
+
+  const handlePwInput = event => {
+    setPassword(event.target.value);
+  };
+  return (
+    <div className="inputBlank">
+      {/* ID */}
+      <input
+        onChange={handleIdInput}
+        className="id"
+        type="text"
+        placeholder="전화번호, 사용자 이름 또는 이메일"
+      />
+      {/* PASSWORD */}
+      <input
+        onChange={handlePwInput}
+        className="password"
+        type="password"
+        placeholder="비밀번호"
+      />
+      {/* LOGIN BUTTON */}
+      <button type="button" className="unactivatedBtn" disabled>
+        로그인
+      </button>
+    </div>
+  );
+};
+
 export default LoginJaeHong;
